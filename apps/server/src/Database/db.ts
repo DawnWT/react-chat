a77e9b01193b9ed9177e7950f885c6fcacf922fc
DawnWT <dwnssb@gmail.com>
2024-02-12 07:04:11 +0100

    feat: init kysely with db type

---
+++ apps/server/src/Database/db.ts
@@ -0,0 +1,22 @@
+import { Kysely, PostgresDialect } from 'kysely'
+import { Pool } from 'pg'
+
+import { env } from '../helpers/env.js'
+import { UserTable } from './Tables/user.js'
+
+interface Database {
+  users: UserTable
+}
+
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    host: env.DATABASE_URL,
+    database: 'chat-app',
+    user: env.DATABASE_USER,
+    password: env.DATABASE_PASSWORD,
+    port: env.DATABASE_PORT,
+    max: 10,
+  }),
+})
+
+export const db = new Kysely<Database>({ dialect })
