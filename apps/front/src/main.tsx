2d81b4ef3e73cee58c4cddaa0ff478eb9b44518c
DawnWT <dwnssb@gmail.com>
2024-02-12 15:38:05 +0100

    feat: init env validator & tanstack query

--- apps/front/src/main.tsx
+++ apps/front/src/main.tsx
@@ -1,11 +1,18 @@
+import './config/env.js'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { App } from './App.js'
 
+const queryClient = new QueryClient()
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 )
