09063bd5d707a9cfc93dfc79b8f4527bfe4a8574
DawnWT <dwnssb@gmail.com>
2024-02-14 09:48:23 +0100

    feat: create useGetUser hook

---
+++ apps/front/src/hooks/useGetUser.ts
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { env } from '../config/env.js'
+
+interface useGetUserProps {
+  id: number
+}
+
+export const useGetUser = function ({ id }: useGetUserProps) {
+  return useQuery<{ id: number; name: string; created_at: Date }>({
+    queryKey: ['useGetUser'],
+    queryFn: () => fetch(`${env.BACKEND_URL}/users/${id}`).then((res) => res.json()),
+  })
+}
