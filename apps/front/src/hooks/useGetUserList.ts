3dfd7d23ffdd519520bb5bce0863313d7324bc34
DawnWT <dwnssb@gmail.com>
2024-02-14 09:48:30 +0100

    feat: create useGetUserList hook

---
+++ apps/front/src/hooks/useGetUserList.ts
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { env } from '../config/env.js'
+
+interface useGetUserListProps {
+  nameLike?: string
+}
+
+export const useGetUserList = function ({ nameLike }: useGetUserListProps) {
+  return useQuery<Array<{ id: number; name: string }>>({
+    queryKey: ['useGetUserList'],
+    queryFn: async () => {
+      const queryParam = nameLike ? `?name=${nameLike}` : ''
+      return fetch(`${env.BACKEND_URL}/users${queryParam}`).then((res) => res.json())
+    },
+  })
+}
