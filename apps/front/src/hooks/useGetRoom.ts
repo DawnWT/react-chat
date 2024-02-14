8255c98e8bf17d47a449a3070653230b07ec29e6
DawnWT <dwnssb@gmail.com>
2024-02-14 10:45:28 +0100

    feat: create useGetRoom hook

---
+++ apps/front/src/hooks/useGetRoom.ts
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { env } from '../config/env.js'
+
+interface useGetRoomProps {
+  roomId: number
+}
+
+export const useGetRoom = function ({ roomId }: useGetRoomProps) {
+  return useQuery<{ id: number; created_at: Date }>({
+    queryKey: ['useGetRoom'],
+    queryFn: () => fetch(`${env.BACKEND_URL}/rooms/${roomId}`).then((res) => res.json()),
+  })
+}
