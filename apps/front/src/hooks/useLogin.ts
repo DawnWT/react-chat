7cada363054756f419734cc578d89e0f019b1cc9
DawnWT <dwnssb@gmail.com>
2024-02-14 10:02:07 +0100

    feat: refactor useAuth to useLogin/useRegister

---
+++ apps/front/src/hooks/useLogin.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { env } from '../config/env.js'
+
+interface useLoginProps {
+  username?: string
+  password?: string
+}
+
+export const useAuthApi = function ({ username, password }: useLoginProps = {}) {
+  return useQuery({
+    queryKey: ['useLogin'],
+    queryFn: () =>
+      fetch(`${env.BACKEND_URL}/auth/login`, { method: 'POST', body: JSON.stringify({ username, password }) }).then(
+        (res) => res.json()
+      ),
+  })
+}
---
+++ apps/front/src/hooks/useRegister.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { env } from '../config/env.js'
+
+interface useRegisterProps {
+  username: string
+  password: string
+}
+
+export const useAuthApi = function ({ username, password }: useRegisterProps) {
+  return useQuery({
+    queryKey: ['useAuth'],
+    queryFn: () =>
+      fetch(`${env.BACKEND_URL}/auth/register`, { method: 'POST', body: JSON.stringify({ username, password }) }).then(
+        (res) => res.json()
+      ),
+  })
+}
