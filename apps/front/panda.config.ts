eaeaa249f83d1c267c05748d0f04d28db29e522b
dwn <dwnssb@gmail.com>
2024-02-15 23:07:59 +0100

    chore: update panda config

--- apps/front/panda.config.ts
+++ apps/front/panda.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@pandacss/dev'
 export default defineConfig({
   preflight: true,
   jsxFramework: 'react',
+  strictPropertyValues: true,
   include: ['src/components/*.{js,jsx,ts,tsx}', 'src/pages/*.{js,jsx,ts,tsx}'],
   exclude: [],
   outdir: 'styled-system',
