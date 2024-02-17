e899fc9f8eb9a7c00c4fc7c0e741e3b415e52374
dwn <dwnssb@gmail.com>
2024-02-17 04:50:57 +0100

    feat: create Card component

---
+++ apps/front/src/components/Card.tsx
@@ -0,0 +1,23 @@
+import { styled, VStack } from '../../styled-system/jsx'
+
+export const Card = styled(VStack, {
+  base: {
+    gap: 4,
+    padding: 5,
+    borderRadius: 10,
+    borderWidth: '1px',
+    borderStyle: 'solid',
+    borderColor: 'gray.300',
+    boxShadow: 'xl',
+  },
+  variants: {
+    align: {
+      start: {
+        alignItems: 'start',
+      },
+      end: {
+        alignItems: 'end',
+      },
+    },
+  },
+})
