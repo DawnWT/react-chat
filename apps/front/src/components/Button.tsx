fe5bb7a03bef46f483ec3029a65e23d39169aa62
dwn <dwnssb@gmail.com>
2024-02-17 04:50:47 +0100

    feat: create Button component

---
+++ apps/front/src/components/Button.tsx
@@ -0,0 +1,31 @@
+import { styled } from '../../styled-system/jsx'
+
+export const Button = styled('button', {
+  base: {
+    paddingX: 4,
+    paddingY: 2,
+    width: '100%',
+    borderRadius: 5,
+    borderWidth: '1px',
+    borderStyle: 'solid',
+    borderColor: 'gray.300',
+    cursor: 'pointer',
+    fontWeight: 'medium',
+    _hover: {
+      backgroundColor: 'gray.200',
+      transition: 'ease-in-out',
+      transitionDuration: '0.2s',
+    },
+    _focus: {
+      backgroundColor: 'gray.300',
+      transition: 'ease-in-out',
+      transitionDuration: '0.2s',
+    },
+    _disabled: {
+      backgroundColor: 'gray.700',
+      transition: 'ease-in-out',
+      transitionDuration: '0.2s',
+      cursor: 'not-allowed',
+    },
+  },
+})
