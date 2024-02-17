a4e6f6809956be2abbafa7fa882d0f83d0344d7e
dwn <dwnssb@gmail.com>
2024-02-17 04:51:52 +0100

    feat: implement layouts & pages to router

--- apps/front/src/config/router.tsx
+++ apps/front/src/config/router.tsx
@@ -1,9 +1,41 @@
-import { createBrowserRouter } from "react-router-dom";
-import { Home } from "../pages/Home.js";
+import { createBrowserRouter } from 'react-router-dom'
+
+import { AuthLayout } from '../pages/AuthLayout.js'
+import { Home } from '../pages/Home.js'
+import { LoginPage } from '../pages/Login.js'
+import { ProtectedLayout } from '../pages/ProtectedLayout.js'
+import { RegisterPage } from '../pages/Register.js'
+import { SetUserLayout } from '../pages/SetUserLayout.js'
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Home />
-    }
+  {
+    path: '/',
+    element: <SetUserLayout />,
+    children: [
+      {
+        path: '',
+        element: <ProtectedLayout />,
+        children: [
+          {
+            path: '',
+            element: <Home />,
+          },
+        ],
+      },
+      {
+        path: '',
+        element: <AuthLayout />,
+        children: [
+          {
+            path: 'login',
+            element: <LoginPage />,
+          },
+          {
+            path: 'register',
+            element: <RegisterPage />,
+          },
+        ],
+      },
+    ],
+  },
 ])
