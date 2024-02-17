8d6f840c40600e808edaddb94c741e4203f50680
dwn <dwnssb@gmail.com>
2024-02-17 23:22:29 +0100

    feat: create import aliases

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  resolve: { alias: { '@src': '/src', '@panda': '/styled-system' } },
   plugins: [react()],
   server: {
     host: true,
