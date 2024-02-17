7174a9d8fc9f9ff4c9396c9db4f15e9fe0658514
dwn <dwnssb@gmail.com>
2024-02-17 16:00:50 +0100

    feat: export pg pool

--- apps/server/src/database/db.ts
+++ apps/server/src/database/db.ts
@@ -7,15 +7,32 @@ import { RoomTable } from './tables/room.js'
 import { UserTable } from './tables/user.js'
 const { Pool } = pkg
 
+// eslint-disable-next-line import/exports-last
+export const pool = new Pool({
+  host: env.DATABASE_URL,
+  database: 'chat-app',
+  user: env.DATABASE_USER,
+  password: env.DATABASE_PASSWORD,
+  port: env.DATABASE_PORT,
+})
+
+pool
+  .query(
+    `
+  CREATE TABLE IF NOT EXISTS socket_io_attachments (
+      id          bigserial UNIQUE,
+      created_at  timestamptz DEFAULT NOW(),
+      payload     bytea
+  );
+`
+  )
+  .catch(() => {
+    console.log('could not create table for Postgres Socket Adapter')
+    process.exit(1)
+  })
+
 const dialect = new PostgresDialect({
-  pool: new Pool({
-    host: env.DATABASE_URL,
-    database: 'chat-app',
-    user: env.DATABASE_USER,
-    password: env.DATABASE_PASSWORD,
-    port: env.DATABASE_PORT,
-    max: 10,
-  }),
+  pool: pool,
 })
 
 export interface Database {
