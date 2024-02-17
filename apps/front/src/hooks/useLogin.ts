744feb1a866b3502d307f1587634dccb482f74ed
dwn <dwnssb@gmail.com>
2024-02-17 04:45:41 +0100

    feat: make login/register hook mutations

--- apps/front/src/hooks/useLogin.ts
+++ apps/front/src/hooks/useLogin.ts
@@ -1,20 +1,55 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation } from '@tanstack/react-query'
+import { useEffect } from 'react'
 
 import { env } from '../config/env.js'
+import { useCurrentUserStore } from '../store/currentUserStore.js'
 
-interface useLoginProps {
-  username?: string
-  password?: string
+interface LoginRouteResponseJson {
+  id: number
+  username: string
+  displayName: string
+  password: string
 }
 
-export const useAuthApi = function ({ username, password }: useLoginProps = {}) {
-  return useQuery({
-    queryKey: ['useLogin'],
-    queryFn: () =>
+interface useLoginProps {
+  onError?: (error: Error, variables: useLoginMutateProps, context: unknown) => unknown
+  onSuccess?: (data: LoginRouteResponseJson, variables: useLoginMutateProps, context: unknown) => unknown
+}
+
+interface useLoginMutateProps {
+  username: string
+  password: string
+}
+
+export const useLogin = function ({ onError = () => void 0, onSuccess = () => void 0 }: useLoginProps = {}) {
+  const mutation = useMutation({
+    mutationKey: ['useLogin'],
+    mutationFn: (formData: useLoginMutateProps) =>
       fetch(`${env.VITE_BACKEND_URL}/auth/login`, {
         method: 'POST',
-        body: JSON.stringify({ username, password }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
         credentials: 'include',
-      }).then((res) => res.json()),
+      }).then((res) => {
+        if (res.ok) {
+          return res.json() as Promise<LoginRouteResponseJson>
+        }
+
+        throw new Error('Invalid username or password')
+      }),
+
+    onSuccess,
+    onError,
   })
+  const { setUser } = useCurrentUserStore()
+
+  useEffect(() => {
+    if (mutation.isSuccess) {
+      setUser(mutation.data.id, mutation.data.username, mutation.data.displayName)
+    }
+  }, [mutation.isPending])
+
+  return mutation
 }
--- apps/front/src/hooks/useRegister.ts
+++ apps/front/src/hooks/useRegister.ts
@@ -1,20 +1,52 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation } from '@tanstack/react-query'
+import { useEffect } from 'react'
 
 import { env } from '../config/env.js'
+import { useCurrentUserStore } from '../store/currentUserStore.js'
 
-interface useRegisterProps {
+interface RegisterRouteResponseJSON {
+  id: number
   username: string
+  displayName: string
   password: string
 }
 
-export const useAuthApi = function ({ username, password }: useRegisterProps) {
-  return useQuery({
-    queryKey: ['useAuth'],
-    queryFn: () =>
+interface useRegisterMutateProps {
+  username: string
+  displayName?: string
+  password: string
+}
+
+interface useRegisterProps {
+  onError?: (error: Error, variables: useRegisterMutateProps, context: unknown) => unknown
+  onSuccess?: (data: RegisterRouteResponseJSON, variables: useRegisterMutateProps, context: unknown) => unknown
+}
+
+export const useRegister = function ({ onError = () => void 0, onSuccess = () => void 0 }: useRegisterProps = {}) {
+  const mutation = useMutation({
+    mutationKey: ['useRegister'],
+    mutationFn: ({ username, displayName, password }: useRegisterMutateProps) =>
       fetch(`${env.VITE_BACKEND_URL}/auth/register`, {
         method: 'POST',
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({ username, displayName, password }),
         credentials: 'include',
-      }).then((res) => res.json()),
+      }).then((res) => {
+        if (!res.ok) {
+          throw new Error('Username already exists')
+        }
+
+        return res.json() as Promise<RegisterRouteResponseJSON>
+      }),
+    onError,
+    onSuccess,
   })
+  const { setUser } = useCurrentUserStore()
+
+  useEffect(() => {
+    if (mutation.isSuccess) {
+      setUser(mutation.data.id, mutation.data.username, mutation.data.displayName)
+    }
+  }, [mutation.isPending])
+
+  return mutation
 }
