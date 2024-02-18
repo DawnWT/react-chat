0fb83e25eb263cdbf7aaa4626c9b19e58f6e912a
ALATORRE <audran.latorre@ynov.com>
2024-02-18 19:52:31 +0100

    fix problem css and reload

--- apps/front/panda.config.ts
+++ apps/front/panda.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   preflight: true,
   jsxFramework: 'react',
   strictPropertyValues: true,
-  include: ['src/components/*.{js,jsx,ts,tsx}', 'src/pages/*.{js,jsx,ts,tsx}'],
+  include: ['./src/components/**/*.{js,jsx,ts,tsx}', './src/pages/**/*.{js,jsx,ts,tsx}'],
   exclude: [],
   outdir: 'styled-system',
 })
