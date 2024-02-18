b92e1bd09051708ce309771f9a01b3876d9a1275
dwn <dwnssb@gmail.com>
2024-02-18 16:53:44 +0100

    fix: Auth layout container

--- apps/front/src/pages/AuthLayout.tsx
+++ apps/front/src/pages/AuthLayout.tsx
@@ -3,7 +3,13 @@ import { Outlet } from 'react-router-dom'
 
 export const AuthLayout = function () {
   return (
-    <Container display="grid" justifyContent="center" alignItems="center" gridTemplateRows="3" height="100%">
+    <Container
+      display="grid"
+      justifyContent="center"
+      alignItems="center"
+      gridTemplateRows="1fr max-content 1fr"
+      height="100%"
+    >
       <Outlet />
     </Container>
   )
