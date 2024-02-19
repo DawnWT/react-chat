c0d072c7a3c0104fb04da436afaa70408e7ff13a
DawnWT <dwnssb@gmail.com>
2024-02-19 11:52:10 +0100

    chore: remove comments

--- apps/front/src/pages/Home/RoomSideBar.tsx
+++ apps/front/src/pages/Home/RoomSideBar.tsx
@@ -1,4 +1,5 @@
 import { css } from '@panda/css'
+import { PrivateMessage } from '@src/components/PrivateMessage'
 import { useGetUserRoomList } from '@src/hooks/useGetUserRoomList'
 import { SocketEvent, useSocket } from '@src/hooks/useSocket'
 import { socket } from '@src/socket'
@@ -6,7 +7,6 @@ import { useCurrentUserStore } from '@src/store/currentUserStore'
 import { useCallback } from 'react'
 
 import { UserSearchBar } from '../../components/UserSearchBar'
-import { PrivateMessage } from '@src/components/PrivateMessage'
 
 export const RoomSideBar = function () {
   const { userId, username } = useCurrentUserStore()
@@ -20,24 +20,20 @@ export const RoomSideBar = function () {
     {
       name: 'room-created',
       handler: () => {
-        console.log('room-created')
         void refetch()
       },
     },
     {
       name: 'room-deleted',
       handler: () => {
-        console.log('room-deleted')
         void refetch()
       },
     },
   ]
 
   const handleRoomClick = (id: number) => {
-    console.log('room clicked', id)
     localStorage.setItem('roomId', id.toString())
   }
-
 
   useSocket(EventList)
   return (
@@ -51,27 +47,17 @@ export const RoomSideBar = function () {
       })}
     >
       <UserSearchBar onConfirm={onConfirmUser} />
-      {/* <ul>
-        {isSuccess &&
-          data.rooms.map((v) => (
-            <li key={v.id}>
-              <span>
-                {v.id} | {v.user1_display_name} | {v.user2_display_name} | {v.content} |{' '}
-              </span>
-              <button onClick={() => socket.emit('room-delete', v.user1_id, v.user2_id, v.id)}>del</button>
-            </li>
-          ))}
-      </ul> */}
       {isSuccess &&
         data.rooms.map((v) => (
           <PrivateMessage
             key={v.id}
             userName={(username === v.user1_display_name ? v.user2_display_name : v.user1_display_name) ?? ''}
             message={v.content || 'No message'}
-            onClick={() => handleRoomClick(v.id)}
+            onClick={() => {
+              handleRoomClick(v.id)
+            }}
           />
-        ))
-      }
+        ))}
     </div>
   )
 }
